d42bf64dc9419190664fe9c8ea2329d504e0bc97
Chris M <webmaster@magnaws.com>
2018-09-30 17:39:05 -0400

    Added the Fastlane stuff to the workspace.

--- fastlane/Appfile.swift
+++ fastlane/Appfile.swift
@@ -1,6 +1,3 @@
-var appIdentifier: String { return "com.littlegreenviper.white-dragon" } // The bundle identifier of your app
-var appleID: String { return "chris@littlegreenviper.com" } // Your Apple email address
-
 var itcTeam: String? { return "1451869" } // App Store Connect Team ID
 var teamID: String { return "P53V4JS928" } // Apple Developer Portal Team ID
 
