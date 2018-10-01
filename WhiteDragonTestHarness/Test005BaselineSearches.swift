a22de8a5d63cbe67153b180fe67705f9e15426f3
Chris M <webmaster@magnaws.com>
2018-10-01 16:38:16 -0400

    Added a bunch of test cases. The real proof comes later, when I add children.

--- WhiteDragonTestHarness/Test005BaselineSearches.swift
+++ WhiteDragonTestHarness/Test005BaselineSearches.swift
@@ -25,8 +25,13 @@ import MapKit
 class Test005BaselineSearches: UIViewController, RVP_Cocoa_SDK_Delegate, UIPickerViewDataSource, UIPickerViewDelegate, UITableViewDelegate, UITableViewDataSource {
     private let _presets: [(name: String, values: [Any])] = [(name: "Single Image", values: [1732]),
                                                              (name: "Single Image and Single Place", values: [2, 1732]),
-                                                             (name: "Single Image, Single Place, and Single User", values: [2, 1725, 1732])
-    ]
+                                                             (name: "Single Image, Single Place, and Single User", values: [2, 1725, 1732]),
+                                                             (name: "Multiple Images", values: [1732, 1733, 1734, 1736, 1739, 1742, 1755]),
+                                                             (name: "Multiple Places", values: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 875, 876, 877, 879, 880, 881, 882, 883, 884, 885, 886, 1640, 1641, 1642, 1643, 1644, 1645, 1646, 1647, 1648, 1700, 1701, 1702, 1703]),
+                                                             (name: "DC Area Admins", values: [1725, 1726, 1727, 1728, 1729, 1730]),
+                                                             (name: "DilbertCo Restricted People", values: [1745, 1746, 1747, 1748, 1749, 1750, 1751, 1752, 1753, 1754]),
+                                                             (name: "DC Area Admins, Images and Places", values: [1725, 1726, 1727, 1728, 1729, 1730, 1732, 1733, 1734, 1736, 1739, 1742, 1755, 885, 886, 1640, 1641, 1642, 1643])
+   ]
     private let _buttonStrings = ["LOGIN", "LOGOUT"]
     private var _objectList: [A_RVP_Cocoa_SDK_Object] = []
     
