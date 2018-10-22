720d1db2df4cfd09d76e86a629beebc5e67324b6
Chris M <webmaster@magnaws.com>
2018-10-22 07:59:29 -0400

    Switched to a static library.

---
+++ WhiteDragonSDK/WhiteDragonSDK.swift
@@ -0,0 +1,11 @@
+//
+//  WhiteDragonSDK.swift
+//  WhiteDragonSDK
+//
+//  Created by Chris Marshall on 10/22/18.
+//  Copyright © 2018 Little Green Viper Software Development LLC. All rights reserved.
+//
+
+class WhiteDragonSDK {
+
+}
